65f76f8198bf4efac7df9f0e48056b7ca519189e
BianJian <biansant@qq.com>
2016-02-26 10:22:55 +0800

    miss add

---
+++ src/aceinternal/netcore/src/CellSessionx.cpp
@@ -0,0 +1,129 @@
+
+#include "CellSessionx.h"
+#include "SessionPoolAssistent.h"
+
+namespace netcore
+{
+
+CellSessionx::CellSessionx()
+: m_guid(0)
+{
+}
+
+CellSessionx::~CellSessionx()
+{
+
+}
+
+int CellSessionx::open(void * p)
+{
+	int result = Sessionx::open(p);
+	if (-1 != result)
+	{
+		notifySessionPool(this, SS_CONNECTED);
+	}
+
+	//m_save_output_pack_info.init(true, "output_file");
+
+	return result;
+}
+
+int CellSessionx::rd_stream()
+{
+	int result = Sessionx::rd_stream();
+	if (-1 == result)
+	{
+		// net close
+		notifySessionPool(this, SS_CLOSE);
+	}
+
+	return result;
+}
+
+void CellSessionx::output(Packet * packet)
+{
+	ACE_GUARD_RETURN(ACE_Thread_Mutex, guard, m_deferred_outputs_mutex, );
+	m_deferred_outputs.push(packet);
+}
+
+int CellSessionx::wt_stream()
+{
+	int result = 0;
+
+	{
+		ACE_GUARD_RETURN(ACE_Thread_Mutex, guard, m_deferred_outputs_mutex, -1);
+		if ((m_deferred_outputs.size() == 0) && (m_outputs.length() == 0))
+		{
+			// no data to send
+			return 1;
+		}
+
+		while (m_deferred_outputs.size() > 0)
+		{
+			Packet * ps = m_deferred_outputs.front();
+			if (ps->stream_size() < m_outputs.space())
+			{
+				m_outputs.copy(ps->stream(), ps->stream_size());
+				m_deferred_outputs.pop();
+				delete ps;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	if (m_outputs.length() > 0)
+	{
+		ssize_t send_n = this->peer().send(m_outputs.rd_ptr(), m_outputs.length());
+		if (send_n <= 0)
+		{
+			int last_error = ACE_OS::last_error();
+			if ((EWOULDBLOCK == last_error) || (EINTR == last_error) || (EAGAIN == last_error))
+			{
+				result = 0;
+			}
+			else
+			{
+				this->shutdown();
+				result = -1;
+			}
+		}
+		else
+		{
+			//m_save_output_pack_info.save(m_outputs.rd_ptr(), send_n);
+
+			m_outputs.rd_ptr(send_n);
+		}
+	}
+
+	if (m_outputs.length() < 500)
+	{
+		m_outputs.crunch();
+	}
+
+	//if (m_outputs.length() == 0)
+	//{
+	//	ACE_GUARD_RETURN(ACE_Thread_Mutex, guard, m_deferred_outputs_mutex, -1);
+	//	if (m_deferred_outputs.size() == 0)
+	//	{
+	//		//this->reactor()->remove_handler(this, ACE_Event_Handler::WRITE_MASK | ACE_Event_Handler::DONT_CALL);
+	//		result = -1;
+	//	}
+	//}
+
+	return result;
+}
+
+uint64 CellSessionx::getGUID()
+{
+	return m_guid;
+}
+
+void CellSessionx::setGUID(uint64 guid)
+{
+	m_guid = guid;
+}
+
+}
---
+++ src/aceinternal/netcore/src/CellSessionx.h
@@ -0,0 +1,49 @@
+
+#ifndef CELL_SESSION_HPP
+#define CELL_SESSION_HPP
+
+#include <ace/Reactor.h>
+#include "Sessionx.h"
+#include "Packet.h"
+
+namespace netcore
+{
+
+class CellSessionx;
+
+struct CellSessionInfo
+{
+	CellSessionInfo()
+	{}
+
+	int reference_no;
+};
+
+class CellSessionx : public Sessionx
+{
+public:
+	CellSessionx();
+	~CellSessionx();
+
+	virtual int open(void * p=0) override;
+	virtual void output(Packet * packet) override; // 有可能 非reactor线程,所以写入缓存,待callback
+
+	virtual int rd_stream() override;
+	virtual int wt_stream() override; // copy m_deferred_outputs to outputs
+
+	uint64 getGUID();
+	void setGUID(uint64 guid);
+
+
+private:
+	uint64 m_guid;
+
+	ACE_Thread_Mutex m_deferred_outputs_mutex;
+	PacketQue_t m_deferred_outputs;
+
+	//SavePackInfo m_save_output_pack_info;
+};
+
+}
+
+#endif
